8794c5697855ef2268ee0abf13480d6c499ec9ef
gabriel.turra@outlook.com <gabriel.turra@outlook.com>
2025-02-16 19:17:03 -0300

    feat(errors): reload button in boundary

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -16,12 +16,18 @@ export default function ErrorBoundary({
   }, [error]);
 
   return (
-    <div className="h-screen flex items-center justify-center max-w-2xl mx-auto">
+    <div className="h-screen flex items-center justify-center max-w-xl mx-auto">
       <Alert variant={'destructive'}>
         <Terminal className="h-4 w-4" />
         <AlertTitle>Nos desculpe, essa página não está funcionando corretamente.</AlertTitle>
         <AlertDescription>
           Você pode retornar a página inicial, em breve corrigiremos isso.
+          <button
+            className="bg-red-500 text-primary-foreground px-2 py-1 my-2 cursor-pointer rounded"
+            type="button"
+            onClick={() => reset()}>
+            Recarregar página
+          </button>
         </AlertDescription>
       </Alert>
     </div>

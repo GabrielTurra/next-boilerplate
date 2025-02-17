cc3916218b7c80e5fa8f04a5ccf40c5b881ad3d1
gabriel.turra@outlook.com <gabriel.turra@outlook.com>
2025-02-17 18:39:16 -0300

    style(lint): lint code

--- src/components/ui/button/button.spec.tsx
+++ src/components/ui/button/button.spec.tsx
@@ -6,9 +6,7 @@ describe('<Button />', () => {
   it('should render the heading', () => {
     const { container } = render(<Button title="Button" />);
 
-    expect(
-      screen.getByRole('heading', { name: /Button/i })
-    ).toBeInTheDocument();
+    expect(screen.getByRole('heading', { name: /Button/i })).toBeInTheDocument();
 
     expect(container.firstChild).toMatchSnapshot();
   });
